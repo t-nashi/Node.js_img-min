ee7ee96e2f1ea4f0911be3a13d70aa4d39f3de28
TsutomuTakanashi <t_19nashi@yahoo.co.jp>
2017-09-15 17:39:02 +0900

    ファイル一式追加

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+var pngquant = require("imagemin-pngquant");
+var mozjpeg  = require("imagemin-mozjpeg");
+
+
+// 画像最適化
+gulp.task('imagemin', function(){
+    gulp.src("./src/images/*.jpg")
+        .pipe(imagemin(
+            [mozjpeg({quality:85, progressive: true})]
+        ))
+        .pipe(gulp.dest("./public/images"));
+    gulp.src("./src/images/*.png")
+        .pipe(imagemin(
+            [pngquant({quality: '40-80', speed: 1})] //40-70
+        ))
+        .pipe(imagemin()) // pngquantで圧縮した画像が暗くなる現象対策（余計なガンマ情報を削除）
+        .pipe(gulp.dest("./public/images"));
+});
